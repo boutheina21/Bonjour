50935d18608f4cb1a0a1cf6a96db50a6991184e3
boutheina21 <benboutheina21@gmail.com>
2016-12-29 22:47:37 +0100

    j'ai modifier le code

--- src/bonjour.java
+++ src/bonjour.java
@@ -1,7 +1,5 @@
-/**
- * Created by H on 16/10/2016.
- */
+
 public class bonjour{
     public static void main (String [] args){
-    System.out.println("bonjour a tous");
+    System.out.println("bonjour tous le monde ");
 }}
